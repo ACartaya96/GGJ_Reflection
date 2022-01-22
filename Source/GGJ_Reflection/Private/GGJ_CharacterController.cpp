2c4983113f9b3641d830a940be03cb266c3f5e9f
ACartaya96 <87090729+ACartaya96@users.noreply.github.com>
2022-01-22 17:06:38 -0500

    Character Controller 1.0

--- Source/GGJ_Reflection/Private/GGJ_CharacterController.cpp
+++ Source/GGJ_Reflection/Private/GGJ_CharacterController.cpp
@@ -10,6 +10,10 @@ AGGJ_CharacterController::AGGJ_CharacterController()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	MyMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cylinder"));
+	MyMeshComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+
 }
 
 // Called when the game starts or when spawned
@@ -18,6 +22,7 @@ void AGGJ_CharacterController::BeginPlay()
 	Super::BeginPlay();
 	
 }
+
 
 // Called every frame
 void AGGJ_CharacterController::Tick(float DeltaTime)
@@ -30,6 +35,29 @@ void AGGJ_CharacterController::Tick(float DeltaTime)
 void AGGJ_CharacterController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	InputComponent-> BindAxis("Horizontal", this, &AGGJ_CharacterController::HorizontalMove);
+	InputComponent-> BindAxis("Vertical", this, &AGGJ_CharacterController::VerticalMove);
+	
+}
+
+void AGGJ_CharacterController::HorizontalMove(float value)
+{
+	if (value)
+	{
+		AddMovementInput(GetActorRightVector(), value);
+	}
+}
+
+void AGGJ_CharacterController::VerticalMove(float value)
+{
+	if (value)
+	{
+		AddMovementInput(GetActorForwardVector(), value);
+	}
+}
+
+void AGGJ_CharacterController::Jump()
+{
 
 }
 
--- Source/GGJ_Reflection/Public/GGJ_CharacterController.h
+++ Source/GGJ_Reflection/Public/GGJ_CharacterController.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/InputComponent.h"
 #include "GameFramework/Character.h"
 #include "GGJ_CharacterController.generated.h"
 
@@ -19,11 +20,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	bool isJumping;
+
 public:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* MyMeshComponent;
+
+	
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	void HorizontalMove(float value);
+	void VerticalMove(float value);
+	void Jump();
+	
 };
